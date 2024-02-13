888aa4f47835195b47e53e6716ba8fe5426b7f54
ashleegaf <ashleegaf@gmail.com>
2024-02-13 15:23:23 -0500

    feat: create an image with annotations

---
+++ src/components/ui/AnnotatedFigure.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+interface IAnnotatedFigure {
+    image: File;
+    isLoading?: boolean;
+}
+
+const AnnotatedFigure: React.FC<IAnnotatedFigure> = ({ image, isLoading = true }) => {
+    const handleClick = async (_event: React.MouseEvent<HTMLElement>) => {
+        if (isLoading) {
+            return;
+        }
+    };
+
+    return (
+        <figure
+            className={`mb-5 bg-black relative ${isLoading ? 'cursor-default' : 'cursor-pointer'}`}
+            onClick={handleClick}
+        >
+            <CanvasImage src={URL.createObjectURL(image)} width={100} height={100} />
+            <figcaption className='absolute top-4 left-1/2 transform -translate-x-1/2 bg-white p-1'>
+                {isLoading ? 'Processing...' : '1 face'}
+            </figcaption>
+        </figure>
+    );
+};
+
+export default AnnotatedFigure;
