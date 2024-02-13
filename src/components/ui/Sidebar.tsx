94f66ade3b0043be1c74777a241627fe3ffbf96e
ashleegaf <ashleegaf@gmail.com>
2024-02-13 15:13:18 -0500

    feat: add an uploaded image to a list for display

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -1,14 +1,24 @@
 'use client';
 
 import { useState } from 'react';
+import FileInput from '@/components/form/FileInput';
 
 const Sidebar = () => {
-    const [thumbnails, setThumbnails] = useState<any[]>([]);
+    const [thumbnails, setThumbnails] = useState<React.ReactNode[]>([]);
+
+    const handleChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (!event.target.files) {
+            return;
+        }
+
+        const image = event.target.files[0];
+        setThumbnails((prev) => [<AnnotatedFigure image={image} key={image.name} />, ...prev]);
+    };
 
     return (
         <div className='overflow-y-auto'>
             <div className='flex flex-col items-center gap-y-6 bg-gray-100 overflow-y-auto pt-5 w-60 h-screen max-h-screen'>
-                <FileInput />
+                <FileInput handleChange={handleChange} />
                 <div>{thumbnails}</div>
             </div>
         </div>
