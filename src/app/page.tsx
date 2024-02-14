3e5893eeffb7e202faec23ff7bab2d63c9fb062a
ashleegaf <ashleegaf@gmail.com>
2024-02-14 11:30:49 -0500

    feat: run face detection processing on image upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,37 @@
 'use client';
 
+import { useEffect, useRef, useState } from 'react';
+import Image from 'next/image';
 import { useImageProcessor } from '@/app/contextProvider';
-import CanvasImage from '@/components/ui/CanvasImage';
 import Sidebar from '@/components/ui/Sidebar';
+import { faceDetectionService } from '@/services/faceDetectionService';
+
+const THUMBNAIL_SIZE = 384; // Corresponds to tailwind CSS container size
 
 const Home = () => {
     const { processedImage } = useImageProcessor();
+    const [isLoadingModel, setIsLoadingModel] = useState<boolean>(false);
+    const [isLoadingDetection, setIsLoadingDetection] = useState<boolean>(false);
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const imageRef = useRef<HTMLImageElement | null>(null);
+
+    useEffect(() => {
+        (async function () {
+            setIsLoadingModel(true);
+            await faceDetectionService.loadModels();
+            setIsLoadingModel(false);
+        })();
+    }, []);
+
+    useEffect(() => {
+        if (processedImage) {
+            (async function () {
+                setIsLoadingDetection(true);
+                await faceDetectionService.detectFaces(imageRef, canvasRef);
+                setIsLoadingDetection(false);
+            })();
+        }
+    }, [processedImage]);
 
     return (
         <div className='min-h-screen flex'>
@@ -14,7 +40,24 @@ const Home = () => {
                 <header>
                     <h1>AI Face Detection Processor</h1>
                 </header>
-                {processedImage && <CanvasImage src={processedImage} width={400} height={400} />}
+                {processedImage && (
+                    <div className='relative h-96 w-96 flex justify-center items-center'>
+                        <Image
+                            ref={imageRef}
+                            src={processedImage}
+                            alt='Original image'
+                            fill
+                            className='absolute max-h-full'
+                        />
+                        <canvas
+                            ref={canvasRef}
+                            width={THUMBNAIL_SIZE}
+                            height={THUMBNAIL_SIZE}
+                            className='absolute max-h-full'
+                        />
+                    </div>
+                )}
+                {isLoadingModel && <p>Loading Model...</p>}
             </main>
         </div>
     );
